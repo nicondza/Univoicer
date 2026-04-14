3679e43540b7155384ac47b7163436fc6f7c1260
nicondza <nicondza@gmail.com>
2026-04-14 11:47:25 -0300

    Adjust root universe rendering for favorites

--- app.js
+++ app.js
@@ -1964,8 +1964,9 @@
         const worldNodes = [];
         const worldLinks = [];
         const childUniverseIds = new Set(Object.keys(state.universeMemberships || {}));
-        const rootUniverseNodes = state.universeNodes.filter(node => !childUniverseIds.has(node.id));
-        const favoriteUniverseNodes = state.universeNodes.filter((node) => node.isFavorite);
+        const rootUniverseNodes = state.universeNodes.filter((node) => !childUniverseIds.has(node.id) || node.isFavorite === true);
+        const rootUniverseNodeIds = new Set(rootUniverseNodes.map((node) => node.id));
+        const favoriteUniverseNodes = state.universeNodes.filter((node) => node.isFavorite && !rootUniverseNodeIds.has(node.id));
         const absorptionEffect = state.mapAbsorptionEffect;
         const absorptionMarkup = absorptionEffect
           ? `
